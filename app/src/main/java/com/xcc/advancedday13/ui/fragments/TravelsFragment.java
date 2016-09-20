2cb15d6b7a420be5d15e1bc6ddcc75a693718af9
9999 <289582077@qq.com>
2016-09-20 15:36:33 +0800

    开始码游记

---
+++ app/src/main/java/com/xcc/advancedday13/ui/fragments/TravelsFragment.java
@@ -0,0 +1,19 @@
+package com.xcc.advancedday13.ui.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xcc.advancedday13.base.BaseFragment;
+
+public class TravelsFragment extends BaseFragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
