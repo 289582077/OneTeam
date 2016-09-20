81d6bffb1e4aa784decf899bcffb98f6dece1128
OneTeam <595756041@qq.com>
2016-09-20 15:41:38 +0800

    获取新数据

---
+++ app/src/main/java/com/xcc/advancedday13/ui/fragments/MyFragment.java
@@ -0,0 +1,20 @@
+package com.xcc.advancedday13.ui.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xcc.advancedday13.base.BaseFragment;
+
+/**
+ * Created by DaXianSheng on 2016/9/20.
+ */
+public class MyFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return layout;
+    }
+}
