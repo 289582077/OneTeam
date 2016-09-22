8af9c3d2294f1eb5f20a99860a429b9ddbce388d
OneTeam <595756041@qq.com>
2016-09-22 10:18:21 +0800

    完善协调者布局功能1.1

---
+++ app/src/main/java/com/xcc/advancedday13/ui/SearchResultActivity.java
@@ -0,0 +1,16 @@
+package com.xcc.advancedday13.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xcc.advancedday13.R;
+import com.xcc.advancedday13.base.BaseActivity;
+
+public class SearchResultActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+    }
+}
